febc2a5be2e3a223db9083a71745a9f6a6a40132
Saddam Hussain <sishack8@gmail.com>
2020-10-07 15:34:58 +0530

    Project Init

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::group(['namespace' => 'Admin'],function(){
+    Route::get('/','LoginController@showLoginForm')->name('admin.login');    
+    Route::post('login', 'LoginController@adminLogin');
+    Route::post('logout', 'LoginController@logout')->name('admin.logout');
+
+    Route::group(['middleware'=>'auth:admin','prefix'=>'admin'],function(){
+        Route::get('/dashboard', 'DashboardController@dashboardView')->name('admin.deshboard');
+        
+        Route::get('/change/password/form', 'LoginController@changePasswordForm')->name('admin.change_password_form');
+        Route::post('/change/password', 'LoginController@changePassword')->name('admin.change_password');
+    });
+});
+
